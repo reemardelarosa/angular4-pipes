e3a27efe49fb23a7f7b9b0ae7869f4b1a570be1b
DELAJ161 <John.X.Dela.Rosa.-ND@disney.com>
2017-05-19 23:36:55 +0800

    Updated sort pipe to be impure

--- src/app/sort.pipe.ts
+++ src/app/sort.pipe.ts
@@ -1,19 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'sort'
+  name: 'sort',
+  pure: false
 })
 export class SortPipe implements PipeTransform {
   transform(value: any, prop: string, sorter: string): any {
     const arrangements = {
-      'asc' : [-1, 1, 0],
-      'desc' : [1, -1, 0]
+      'asc' : [1, -1],
+      'desc' : [-1, 1]
     };
     const arr = arrangements[sorter];
-    console.log(arr);
+
     value.sort(
-      function(a, b) {
-        return (a[prop] < b[prop]) ? arr[0] : ((a[prop] > b[prop]) ? arr[1] : arr[2]);
+      (a, b) => {
+        return (a[prop] > b[prop]) ? arr[0] : arr[1];
       }
     );
     return value;
